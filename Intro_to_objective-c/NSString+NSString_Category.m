2e82e700ba5e0371c4ebaf797008b7b38f7c068b
Luay Younus <luayghazwan@yahoo.com>
2017-04-17 17:27:40 -0700

    Instance method that returns the Array of words in a String Method that is a class method that takes in an NSString and returns the same NSString but reversed.

---
+++ Intro_to_objective-c/NSString+NSString_Category.m
@@ -0,0 +1,37 @@
+//
+//  NSString+NSString_Category.m
+//  Intro_to_objective-c
+//
+//  Created by Luay Younus on 4/17/17.
+//  Copyright © 2017 Luay Younus. All rights reserved.
+//
+
+#import "NSString+NSString_Category.h"
+
+@implementation NSString (NSString_Category)
+
+NSString *seperator = @" ";
+
+-(NSArray *) stringArray{
+    NSArray *words = [self componentsSeparatedByString: seperator];
+    return words;
+}
+
++(NSString *) reverseStringMethod : (NSString *) myString{
+    
+    NSString *result = [NSMutableString stringWithString: @""];
+    
+    NSUInteger length = [myString length];
+    
+    NSUInteger index;
+    
+    for (index = length-1 ; index > 0 ; index--){
+        unichar character = [myString characterAtIndex:index];
+        result = [result stringByAppendingFormat:@"%c", character];
+        
+    }
+
+    return result;
+}
+
+@end
